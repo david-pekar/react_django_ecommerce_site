5c47ebf40e2bb671113d9e2bb4bced41f7cd0c05
David Pekar <davidpekar@utexas.edu>
2025-10-07 18:49:49 -0400

    Convert frontend from submodule to normal folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/hello/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+  return (
+    <div>
+      <h1>Hello World from React</h1>
+      <p>Backend says: {message}</p>
+    </div>
+  );
+}
+/*** 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+  ***/
+
+export default App;
